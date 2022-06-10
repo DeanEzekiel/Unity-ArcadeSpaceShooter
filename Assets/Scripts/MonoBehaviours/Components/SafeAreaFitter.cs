d0c9428c6c763767e936fab428fa1a607348260f
Dean Ezekiel Salazar <deanezekiel.salazar@gmail.com>
2022-06-10 19:00:18 +0800

    [MOD] Safe Area update when orientation changed

--- Assets/Scripts/MonoBehaviours/Components/SafeAreaFitter.cs
+++ Assets/Scripts/MonoBehaviours/Components/SafeAreaFitter.cs
@@ -5,7 +5,58 @@ using UnityEngine;
 [RequireComponent(typeof(RectTransform))]
 public class SafeAreaFitter : MonoBehaviour
 {
-    private void Awake()
+    private DeviceOrientation cachedOrientation = DeviceOrientation.Unknown;
+    private ScreenOrientation screenOrientation = ScreenOrientation.Landscape;
+
+    [SerializeField]
+    private GameObject indicator;
+
+    private void Start()
+    {
+        //indicator.SetActive(false);
+    }
+
+    private void Update()
+    {
+#if (UNITY_IOS || UNITY_ANDROID)
+        // USING DEVICE ORIENTATION
+        if ((Input.deviceOrientation == DeviceOrientation.LandscapeLeft) &&
+            (cachedOrientation != DeviceOrientation.LandscapeLeft))
+        {
+            ChangedOrientation();
+            cachedOrientation = DeviceOrientation.LandscapeLeft;
+
+            //StartCoroutine(C_Indicator());
+        }
+        else if ((Input.deviceOrientation == DeviceOrientation.LandscapeRight) &&
+            (cachedOrientation != DeviceOrientation.LandscapeRight))
+        {
+            ChangedOrientation();
+            cachedOrientation = DeviceOrientation.LandscapeRight;
+
+            //StartCoroutine(C_Indicator());
+        }
+#endif
+
+        //// USING SCREEN ORIENTATION
+        //if ((Screen.orientation == ScreenOrientation.LandscapeLeft) &&
+        //    (screenOrientation != ScreenOrientation.LandscapeLeft))
+        //{
+        //    ChangedOrientation();
+        //    screenOrientation = ScreenOrientation.LandscapeLeft;
+        //}
+        //else if ((Screen.orientation == ScreenOrientation.LandscapeRight) &&
+        //    (screenOrientation != ScreenOrientation.LandscapeRight))
+        //{
+        //    ChangedOrientation();
+        //    screenOrientation = ScreenOrientation.LandscapeRight;
+        //}
+
+        //// JUST Check if the screen orientation changed every frame for smoother checking
+        //ChangedOrientation();
+    }
+
+    private void ChangedOrientation()
     {
         var rectTransform = GetComponent<RectTransform>();
         var safeArea = Screen.safeArea;
@@ -25,5 +76,13 @@ public class SafeAreaFitter : MonoBehaviour
 
         rectTransform.anchorMin = anchorMin;
         rectTransform.anchorMax = anchorMax;
+    }
+
+    private IEnumerator C_Indicator()
+    {
+        indicator.SetActive(true);
+        yield return new WaitForSeconds(3);
+
+        indicator.SetActive(false);
     }
 }
